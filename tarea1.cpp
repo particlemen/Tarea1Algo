9ef554c5f760208463dc136694e3fce8e6534825
pvillalon <pablo.villalonc.14@sansano.usm.cl>
2018-10-30 20:14:47 -0300

    aaaaah!

--- tarea1.cpp
+++ tarea1.cpp
@@ -2,18 +2,23 @@
 
 std::string PerRec(int izq, int der, std::string CurPer, std::string &Original, int pos){
 	int n_hijos = CurPer.size() + 1;
-
 	if (izq == der) {
 		return CurPer;
 	}
-	else {
 
-	}
+}
 
+double factorial(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
 }
 
 int main()
 {
 	std::string Perro = "Guau\n";
-  	std::cout << Perro.replace(2,1, "");
+  std::cout << Perro.replace(2,1, "");
+	std::cout << factorial(32);
 }
